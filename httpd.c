3263ae5da85b0d768952654808853aea0bf7dd7b
stone <wansitong11@126.com>
2021-10-09 11:28:06 -0400

    master

--- httpd.c
+++ httpd.c
@@ -13,6 +13,15 @@
 #include <stdlib.h>
 
 #define isSpace(x) isspace((int)x) //宏
+#define SERVER_STRING "Stone's http\r\n"
+
+void* accept_request(void*);
+int get_line(int, char*, int);
+void implemented(int);
+void not_found(int);
+void serve_file(int, const char*);
+void execute_cgi(int, const char*, const char*, const char*);
+void unimplemented(int);
 
 void* accept_request(void* client_sock)
 {
@@ -23,10 +32,10 @@ void* accept_request(void* client_sock)
     char url[255]; //请求的url
     char path[512]; //url中请求的文件路径
     int cgi = 0; //判断是否是cgi
-    char* query_string; //字符指针
+    char* query_string; //字符指针, 指向 ？ 后面的请求内容
     struct stat st;
 
-    numchars = get_line(client, buf, size(buf)); //读取一行请求，放入buf里面
+    numchars = get_line(client, buf, sizeof(buf)); //读取一行请求，放入buf里面
     
     size_t i, j;
     i = 0;
@@ -86,12 +95,105 @@ void* accept_request(void* client_sock)
         while(numchars > 0 && strcmp("\n", buf)){
             numchars = get_line(client, buf, sizeof(buf));
         }
-        not_fount(client);
+        not_found(client);
     }
+    else{
+        //请求参数为目录，则自动打开test.html
+        if((st.st_mode & S_IFMT) == S_IFDIR){ //请求参数为目录
+            strcat(path, "/test.html");
+        }
+        //S_IXUSR:文件所有者具可执行权限
+		//S_IXGRP:用户组具可执行权限
+		//S_IXOTH:其他用户具可读取权限  
+        if((st.st_mode & S_IXUSR) ||
+           (st.st_mode & S_IXGRP) ||
+           (st.st_mode & S_IXOTH))
+        {
+            cgi = 1;
+        }
 
+        if(!cgi) serve_file(client, path);
+        else execute_cgi(client, path, method, query_string);
+    }
+    close(client);
+    return NULL;
+}
 
+int get_line(int sock, char* buf, int size){
+    int i = 0;
+    char c = '\0';
+    int n;
 
+    while(i < size - 1 && c != '\n'){
+        n = recv(sock, &c, 1, 0);
+        if(n > 0){
+            if(c == '\r'){
+                //只把数据读出来，但是不从缓冲区释放
+                //如果是linux的话，每行结尾就是 \r,因此如果直接读的话就会读到下一行
+                //这样就会造成数据的不正确
+                n = recv(sock, &c, 1, MSG_PEEK);
+                if(n > 0 && c == '\n'){
+                    recv(sock, &c, 1, 0);
+                }
+                else{
+                    c = '\n';
+                }
+            }
+            buf[i] = c;
+            i++;
+        }
+        //读到最后一行
+        else{
+            c = '\n';
+        }
+    }
+    buf[i] = '\0';
+    return i;
+}
 
+void not_found(int client){
+    char buf[1024];
+    //返回404
+    sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<BODY><P>The server could not fulfill\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "your request because the resource specified\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "is unavailable or nonexistent.\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "</BODY></HTML>\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+void unimplemented(int client)
+{
+	char buf[1024];
+	//发送501说明相应方法没有实现
+	sprintf(buf, "HTTP/1.0 501 Method Not Implemented\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, SERVER_STRING);
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "Content-Type: text/html\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "</TITLE></HEAD>\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
+	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "</BODY></HTML>\r\n");
+	send(client, buf, strlen(buf), 0);
 }
 
 void error_die(const char* sc)
